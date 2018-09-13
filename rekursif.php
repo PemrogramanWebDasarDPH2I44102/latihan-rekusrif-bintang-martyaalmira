070fb41344cd27e093b440a7ee7843e173778ee4
martyaalmira <42663061+martyaalmira@users.noreply.github.com>
2018-09-13 08:59:38 +0700

    Update rekursif.php

--- rekursif.php
+++ rekursif.php
@@ -1,10 +1,44 @@
+
+
 <?php
 
-function rekursif($i){
-  echo "$i <br>";
-  $i--;
-  if($i>0)
-    rekursif($i);
+echo "1. Piramida Angka<br>"
+$batas=5;
+function rekursif($a =1){
+ global $batas;
+ echo"*";
+  $a++;
+  if($a<=$batas){
+  	 rekursif($a);
+  }
+  else{
+  	echo "<br>";
+  	$batas--;
+  	if ($batas!=0){
+  		rekursif();
+  	}
+  }
 }
-rekursif(5);
+rekursif();
+
+echo "================<br>";
+
+echo "2.Bintang Piramida<br>";
+$batasan=1;
+function rekursifb($i =1){
+ global $batasan;
+ echo"$i";
+  $i++;
+  if($i<=$batasan){
+  	 rekursifb($i);
+  }
+  else{
+  	echo "<br>";
+  	$batasan++;
+  	if ($batasan!=0){
+  		rekursifb();
+  	}
+  }
+}
+rekursifb();
 ?>
